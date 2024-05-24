b6d8b5e71414bc7073896749cc91c36d2ef31de1
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-05-23 22:49:54 -0400

    Criação do projeto calculadora_javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-;
+let display = document.getElementById('display');
+let valorAtual = '';
+let operacaoAnterior = null;
+
+function adicionarNumero(numero) {
+    valorAtual += numero;
+    display.value = valorAtual;
+}
+
+function adicionarOperador(operador) {
+    if (valorAtual === '') {
+        return;
+    }
+
+    if (operacaoAnterior !== null) {
+        calcular();
+    }
+
+    operacaoAnterior = operador;
+    valorAtual = '';
+    display.value = valorAtual;
+}
+
+function calcular() {
+    if (valorAtual === '') {
+        return;
+    }
+
+    let resultado = parseFloat(valorAtual);
+
+    if (operacaoAnterior === '+') {
+        resultado += parseFloat(valorAnterior);
+    } else if (operacaoAnterior === '-') {
+        resultado -= parseFloat(valorAnterior);
+    } else if (operacaoAnterior === '*') {
+        resultado *= parseFloat
+    }
+}
